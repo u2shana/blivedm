039c704cb5d89e5f7e112ef476664ef7bf1681ea
u2shana <845169902@qq.com>
2026-04-25 04:30:27 +0800

    同步上游

--- blivedm/clients/ws_base.py
+++ blivedm/clients/ws_base.py
@@ -15,10 +15,6 @@ from datetime import datetime
 
 logger = logging.getLogger('blivedm')
 
-USER_AGENT = (
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'
-)
-
 HEADER_STRUCT = struct.Struct('>I2H2I')
 
 
